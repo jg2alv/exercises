09378678d8aa8c71a972c65bd4d0129f8c3b402c
João Vaz <joao.guimaraes.alves.vaz@gmail.com>
2019-10-03 19:25:51 -0300

    Add leaderboard

--- php-do-zero-ao-profissional/estourar-baloes/js/script.js
+++ php-do-zero-ao-profissional/estourar-baloes/js/script.js
@@ -1,17 +1,30 @@
-function addBallon() {
-    const ballon = document.createElement("div");
-    ballon.setAttribute("class", "ballon");
-    var browserWidth = document.getElementsByTagName("body")[0].offsetWidth;
-    var browserHeight = document.getElementsByTagName("body")[0].offsetHeight;
-    p1 = Math.floor(Math.random() * browserWidth);
-    p2 = Math.floor(Math.random() * browserHeight);
+function addBalloon() {
+    const balloon = document.createElement("div");
+    balloon.setAttribute("class", "balloon");
+    let browserWidth = document.getElementsByTagName("body")[0].offsetWidth;
+    let browserHeight = document.getElementsByTagName("body")[0].offsetHeight;
+    let p1 = Math.floor(Math.random() * browserWidth);
+    let p2 = Math.floor(Math.random() * browserHeight);
     const hexMax = 256 * 256 * 256;
-    var color = '#' + Math.floor(Math.random() * hexMax).toString(16).toUpperCase().padStart(6, '0');
-    ballon.setAttribute("style", "top: " + p2 + "px; left: " + p1 + "px; background: " + color + ";");
-    ballon.setAttribute("onclick", "removeBallon(this)");
-    document.body.appendChild(ballon);
+    let color = '#' + Math.floor(Math.random() * hexMax).toString(16).toUpperCase().padStart(6, '0');
+    balloon.setAttribute("style", "top: " + p2 + "px; left: " + p1 + "px; background: " + color + ";");
+    balloon.setAttribute("onclick", "removeBalloon(this)");
+    document.body.appendChild(balloon);
+    document.querySelector(".balloons-on-screen h2").innerHTML = document.getElementsByClassName("balloon").length;
+    let created = document.querySelector(".created-balloons h2").innerHTML;
+    if (created == "#") {
+        document.querySelector(".created-balloons h2").innerHTML = 1;
+    } else {
+        document.querySelector(".created-balloons h2").innerHTML = parseInt(document.querySelector(".created-balloons h2").innerHTML) + 1;
+    }
 }
 
-function removeBallon(ballon) {
-    document.body.removeChild(ballon);
+function removeBalloon(balloon) {
+    document.body.removeChild(balloon);
+    let popped = document.querySelector(".popped-balloons h2").innerHTML;
+    if (popped == "#") {
+        document.querySelector(".popped-balloons h2").innerHTML = 1;
+    } else {
+        document.querySelector(".popped-balloons h2").innerHTML = parseInt(document.querySelector(".popped-balloons h2").innerHTML) + 1;
+    }
 }
