e5d79fb89840da6a1d5ffd598ac38222f81c2386
João Vaz <joao.guimaraes.alves.vaz@gmail.com>
2019-10-05 16:32:15 -0300

    Remove files

--- javascript-nivel-1/relogio/v1 (by b7web)/js/script.js
+++
@@ -1,8 +0,0 @@
-function updateTimer() {
-    let h = new Date().getHours();
-    let m = new Date().getMinutes();
-    let s = new Date().getSeconds();
-    document.querySelectorAll(".timer")[0].innerHTML = h;
-    document.querySelectorAll(".timer")[1].innerHTML = m;
-    document.querySelectorAll(".timer")[2].innerHTML = s;
-}
