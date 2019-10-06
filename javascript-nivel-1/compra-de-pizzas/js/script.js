c9925d611e455928fde21a794c68aed6be7c4f3d
João Vaz <joao.guimaraes.alves.vaz@gmail.com>
2019-10-06 17:28:48 -0300

    Final commit

--- javascript-nivel-1/compra-de-pizzas/js/script.js
+++ javascript-nivel-1/compra-de-pizzas/js/script.js
@@ -67,15 +67,84 @@ cs(".pizzaInfo--size").forEach((size) => {
     });
 });
 
+c(".menu-openner").addEventListener("click", () => {
+    if (cart.length > 0) {
+        c("aside").style.left = "0";
+    };
+});
+
+c(".menu-closer").addEventListener("click", () => {
+    c("aside").style.left = "100vw";
+});
+
 c(".pizzaInfo--addButton").addEventListener("click", () => {
     let size = parseInt(c(".pizzaInfo--size.selected").getAttribute("data-key"));
     let id = `${pizzaJson[modalIndex].id}@${size}`;
-    let key = cart.findIndex((item) => item.idCode = id);
-    cart.push({
-        idCode: id,
-        id: pizzaJson[modalIndex].id,
-        size,
-        qt: modalQt
-    })
+    let key = cart.findIndex((item) => item.idCode == id);
+    if (key > -1) {
+        cart[key].qt += modalQt;
+    } else {
+        cart.push({
+            idCode: id,
+            id: pizzaJson[modalIndex].id,
+            size,
+            qt: modalQt
+        })
+    }
+    updateCart();
     closeModal();
 });
+
+function updateCart() {
+    c(".menu-openner span").innerHTML = cart.length;
+    if (cart.length > 0) {
+        c("aside").classList.add("show");
+        c(".cart").innerHTML = "";
+        let subtotal = 0;
+        let total = 0;
+        let desconto = 0;
+        for (let i in cart) {
+            let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id);
+            subtotal += pizzaItem.price * cart[i].qt;
+            let cartItem = c(".models .cart--item").cloneNode(true);
+            let pizzaName;
+            switch (cart[i].size) {
+                case 0:
+                    pizzaName = `${pizzaItem.name} (P)`
+                    break;
+                case 1:
+                    pizzaName = `${pizzaItem.name} (M)`
+                    break;
+                case 2:
+                    pizzaName = `${pizzaItem.name} (G)`
+                    break;
+            }
+            cartItem.querySelector("img").src = pizzaItem.img;
+            cartItem.querySelector(".cart--item-nome").innerHTML = pizzaName;
+            cartItem.querySelector(".cart--item--qt").innerHTML = cart[i].qt;
+            cartItem.querySelector(".cart--item-qtmenos").addEventListener("click", () => {
+                if (cart[i].qt > 1) {
+                    cart[i].qt--;
+
+                } else {
+                    cart.splice(i, 1)
+                }
+                updateCart();
+            });
+
+            cartItem.querySelector(".cart--item-qtmais").addEventListener("click", () => {
+                cart[i].qt++;
+                updateCart();
+            });
+            c(".cart").append(cartItem);
+        }
+        desconto = subtotal * 0.1;
+        total = subtotal - desconto;
+        c(".subtotal span:last-child").innerHTML = `R$ ${subtotal.toFixed(2)}`;
+        c(".desconto span:last-child").innerHTML = `R$ ${desconto.toFixed(2)}`;
+        c(".total span:last-child").innerHTML = `R$ ${total.toFixed(2)}`;
+    } else {
+        c("aside").classList.remove("show");
+        c("aside").style.left = "100vw";
+    }
+}
