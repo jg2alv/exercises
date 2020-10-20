96df3b1a5dcedbaae0da6346b1649d1be148b406
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-10-19 21:55:30 -0300

    Add resistor color exercise

---
+++ exercism/csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Linq;
+
+public static class ResistorColor
+{
+    public static int ColorCode(string color) => (new string[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" }).ToList().IndexOf(color);
+
+    public static string[] Colors() => new string[] { "black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white" };
+}
