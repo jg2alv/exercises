4cbe79355e65edfd4d948a6fb8e1ad4ae90b6367
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-10-21 14:28:02 -0300

    Solve JS/gigasecond

---
+++ exercism/javascript/gigasecond/gigasecond.js
@@ -0,0 +1,6 @@
+//
+// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const gigasecond = date => new Date(date.getTime() + 1000000000000);
