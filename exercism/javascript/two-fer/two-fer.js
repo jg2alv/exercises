92679d67c60569db90f34ea3ba502554a18efe45
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-10-20 21:02:21 -0300

    Solved JS/hello_world and JS/two_fer

---
+++ exercism/javascript/two-fer/two-fer.js
@@ -0,0 +1,6 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const twoFer = name => name ? `One for ${name}, one for me.` : `One for you, one for me.`;
